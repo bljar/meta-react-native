27b5fbdc2011f8ba02b88c8c70b1916abcffa802
Belajar <bljkmp@gmail.com>
2023-07-18 22:12:10 +0700

    Create Footer Component

---
+++ M1L1-Exercise-Your-first-React-Native-component/components/LittleLemonFooter.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function LittleLemonFooter() {
+  return (
+    <View>
+      <Text>All rights reserved by Little Lemon, 2022</Text>
+    </View>
+  );
+}
