e5d25f31a5a7dcf090d5181175742cd2d1b5a479
Belajar <bljkmp@gmail.com>
2023-07-18 22:18:02 +0700

    Style Footer Component

--- M1L1-Exercise-Your-first-React-Native-component/components/LittleLemonFooter.js
+++ M1L1-Exercise-Your-first-React-Native-component/components/LittleLemonFooter.js
@@ -3,7 +3,16 @@ import { View, Text } from "react-native";
 export default function LittleLemonFooter() {
   return (
     <View>
-      <Text>All rights reserved by Little Lemon, 2022</Text>
+      <Text
+        style={{
+          padding: 40,
+          fontSize: 30,
+          color: "Black",
+          textAlign: "center",
+        }}
+      >
+        All rights reserved by Little Lemon, 2022
+      </Text>
     </View>
   );
 }
