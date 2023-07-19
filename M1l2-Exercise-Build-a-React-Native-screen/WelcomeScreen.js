a9b8c343fab8ca558dedf6d5953785963f696814
Belajar <bljkmp@gmail.com>
2023-07-19 17:41:49 +0700

    Create a Welcome Screen

---
+++ M1l2-Exercise-Build-a-React-Native-screen/WelcomeScreen.js
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+export default function WelcomeScreen() {
+  return (
+    <View>
+      <Text>Welcome to Little Lemon</Text>
+      <Text>
+        Little Lemon is a charming neighborhood bistro that serves simple food
+        and classic cocktails in a lively but casual environment. We would love
+        to hear more about your experience with us!
+      </Text>
+    </View>
+  );
+}
