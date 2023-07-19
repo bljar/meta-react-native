1efcfc4872908764cb88c0bae20fe47f20707d2b
Belajar <bljkmp@gmail.com>
2023-07-19 18:07:27 +0700

    Style Welcome Screen

--- M1l2-Exercise-Build-a-React-Native-screen/WelcomeScreen.js
+++ M1l2-Exercise-Build-a-React-Native-screen/WelcomeScreen.js
@@ -2,9 +2,26 @@ import { Text, View } from "react-native";
 
 export default function WelcomeScreen() {
   return (
-    <View>
-      <Text>Welcome to Little Lemon</Text>
-      <Text>
+    <View style={{ flex: 1 }}>
+      <Text
+        style={{
+          padding: 40,
+          fontSize: 30,
+          color: "#EDEFEE",
+          textAlign: "center",
+        }}
+      >
+        Welcome to Little Lemon
+      </Text>
+      <Text
+        style={{
+          padding: 20,
+          marginVertical: 10,
+          color: "white",
+          fontSize: 20,
+          textAlign: "center",
+        }}
+      >
         Little Lemon is a charming neighborhood bistro that serves simple food
         and classic cocktails in a lively but casual environment. We would love
         to hear more about your experience with us!
