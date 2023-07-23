1bb0499763e66b83bbb1cb60f26f16696d3dc3ac
Belajar <bljkmp@gmail.com>
2023-07-23 23:25:55 +0700

    Style the component

--- M3L2-Exercise-Displaying-Images-in-your-app/WelcomeScreen.js
+++ M3L2-Exercise-Displaying-Images-in-your-app/WelcomeScreen.js
@@ -1,20 +1,23 @@
-import * as React from 'react';
-import { ScrollView, Text, StyleSheet } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, Image } from 'react-native';
 
 export default function WelcomeScreen() {
   return (
-    <ScrollView indicatorStyle="white" style={styles.container}>
-    <Image
-    source={require('./img/logo.png')}
-    resizeMode="cover"
-    accessible={true}
-    accessibilityLabel={'Little Lemon Logo'}
-  />
-      <Text style={styles.headerText}>Welcome to Little Lemon</Text>
+    <ScrollView style={styles.container}>
+      <View style={styles.headerWrapper}>
+        <Image
+          style={styles.image}
+          source={require('./img/logo.png')}
+          resizeMode="cover"
+          accessible={true}
+          accessibilityLabel={'Little Lemon Logo'}
+        />
+
+        <Text style={styles.headerText}>Little Lemon</Text>
+      </View>
       <Text style={styles.regularText}>
         Little Lemon is a charming neighborhood bistro that serves simple food
         and classic cocktails in a lively but casual environment. We would love
-        to hear more about your experience with us!
+        to hear your experience with us!
       </Text>
     </ScrollView>
   );
@@ -24,8 +27,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  headerWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: 10,
+  },
   headerText: {
-    padding: 40,
+    paddingRight: 10,
+    paddingLeft: 20,
+    paddingTop: 30,
+    paddingBottom: 10,
     fontSize: 30,
     color: '#EDEFEE',
     textAlign: 'center',
@@ -36,5 +47,10 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     color: '#EDEFEE',
     textAlign: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 20,
   },
 });
