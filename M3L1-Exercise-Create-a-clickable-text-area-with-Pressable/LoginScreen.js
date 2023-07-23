0503e9e83d3d336e87f7192f611a27fe3626deb9
Belajar <bljkmp@gmail.com>
2023-07-23 16:27:49 +0700

    Track logged-in status using local state and update UI if logged in and style component

--- M3L1-Exercise-Create-a-clickable-text-area-with-Pressable/LoginScreen.js
+++ M3L1-Exercise-Create-a-clickable-text-area-with-Pressable/LoginScreen.js
@@ -5,34 +5,45 @@ import {
   StyleSheet,
   TextInput,
   Pressable,
+  View,
 } from "react-native";
 
 export default function LoginScreen() {
   const [email, onChangeEmail] = useState("");
   const [password, onChangePassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.headerText}>Welcome to Little Lemon</Text>
-      <Text style={styles.regularText}>Login to continue </Text>
-      <TextInput
-        style={styles.inputBox}
-        value={email}
-        onChangeText={onChangeEmail}
-        placeholder={"email"}
-        keyboardType={"email-address"}
-      />
-      <TextInput
-        style={styles.inputBox}
-        value={password}
-        onChangeText={onChangePassword}
-        placeholder={"password"}
-        keyboardType={"default"}
-        secureTextEntry={true}
-      />
-      <Pressable>
-        <Text>Login</Text>
-      </Pressable>
+      {!isLoggedIn ? (
+        <View>
+          <Text style={styles.regularText}>Login to continue </Text>
+          <TextInput
+            style={styles.inputBox}
+            value={email}
+            onChangeText={onChangeEmail}
+            placeholder={"email"}
+            keyboardType={"email-address"}
+          />
+          <TextInput
+            style={styles.inputBox}
+            value={password}
+            onChangeText={onChangePassword}
+            placeholder={"password"}
+            keyboardType={"default"}
+            secureTextEntry={true}
+          />
+          <Pressable
+            style={styles.button}
+            onPress={() => setIsLoggedIn(!isLoggedIn)}
+          >
+            <Text style={styles.buttonText}>Login</Text>
+          </Pressable>
+        </View>
+      ) : (
+        <Text style={styles.regularText}>You are logged in!</Text>
+      )}
     </ScrollView>
   );
 }
@@ -62,5 +73,20 @@ const styles = StyleSheet.create({
     fontSize: 16,
     borderColor: "#EDEFEE",
     backgroundColor: "#EDEFEE",
+  },
+  button: {
+    fontSize: 22,
+    padding: 10,
+    marginVertical: 8,
+    margin: 40,
+    backgroundColor: "#EDEFEE",
+    borderColor: "#EDEFEE",
+    borderWidth: 2,
+    borderRadius: 12,
+  },
+  buttonText: {
+    color: "#333333",
+    textAlign: "center",
+    fontSize: 32,
   },
 });
