b7a49eb531b29841e7eb878abaa947cecaccc51e
Belajar <bljkmp@gmail.com>
2023-07-24 18:41:05 +0700

    Instantiate native stack navigator

--- M4L1-Exercise-Set-up-Routes/App.js
+++ M4L1-Exercise-Set-up-Routes/App.js
@@ -1,20 +1,27 @@
-import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import * as React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-import LittleLemonHeader from './components/LittleLemonHeader';
-import LittleLemonFooter from './components/LittleLemonFooter';
-import WelcomeScreen from './WelcomeScreen';
-import LoginScreen from './LoginScreen';
+import LittleLemonHeader from "./components/LittleLemonHeader";
+import LittleLemonFooter from "./components/LittleLemonFooter";
+import WelcomeScreen from "./WelcomeScreen";
+import LoginScreen from "./LoginScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <>
-        <View style={styles.container}>
-          <LittleLemonHeader />
-        </View>
-        <View style={styles.footerContainer}>
-          <LittleLemonFooter />
-        </View>
+      <View style={styles.container}>
+        <LittleLemonHeader />
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Login"></Stack.Navigator>
+        </NavigationContainer>
+      </View>
+      <View style={styles.footerContainer}>
+        <LittleLemonFooter />
+      </View>
     </>
   );
 }
@@ -22,8 +29,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#333333',
+    backgroundColor: "#333333",
   },
-  footerContainer: { backgroundColor: '#333333' },
-});   
-
+  footerContainer: { backgroundColor: "#333333" },
+});
