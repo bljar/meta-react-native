0b3d777288a35c9d8eeeb103f225e2ad6dcd1bbe
Belajar <bljkmp@gmail.com>
2023-07-25 10:52:58 +0700

    Configure the Login button to move from the Login screen to the Welcome screen

---
+++ M4L2-Exercise-Moving-between-screens/App.js
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+import LittleLemonHeader from "./components/LittleLemonHeader";
+import LittleLemonFooter from "./components/LittleLemonFooter";
+import WelcomeScreen from "./WelcomeScreen";
+import LoginScreen from "./LoginScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <>
+      <View style={styles.container}>
+        <LittleLemonHeader />
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Welcome" component={WelcomeScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
+      <View style={styles.footerContainer}>
+        <LittleLemonFooter />
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#333333",
+  },
+  footerContainer: { backgroundColor: "#333333" },
+});
