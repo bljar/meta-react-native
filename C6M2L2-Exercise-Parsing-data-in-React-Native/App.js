400e84213ac6905a00a58ae3374285703ff939de
Belajar <bljkmp@gmail.com>
2023-07-27 11:07:48 +0700

    Style the component

--- C6M2L2-Exercise-Parsing-data-in-React-Native/App.js
+++ C6M2L2-Exercise-Parsing-data-in-React-Native/App.js
@@ -34,6 +34,17 @@ const menuStyles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  innerContainer: {
+    paddingHorizontal: 40,
+    paddingVertical: 20,
+    backgroundColor: "#495E57",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  itemText: {
+    color: "#F4CE14",
+    fontSize: 22,
+  },
   headerText: {
     color: "#F4CE14",
     fontSize: 30,
