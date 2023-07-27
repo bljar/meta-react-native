4deea5b6ba7fb7c0400a7514e4d9cae6c6bb3a7a
Belajar <bljkmp@gmail.com>
2023-07-27 10:20:28 +0700

    HTTP request-response in React Native

--- C6M2L1-Exercise-HTTP-RequestResponse/App.js
+++ C6M2L1-Exercise-HTTP-RequestResponse/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
   ActivityIndicator,
   FlatList,
@@ -6,20 +6,52 @@ import {
   View,
   StyleSheet,
   SafeAreaView,
-} from 'react-native';
+} from "react-native";
 
 export default App = () => {
   const [isLoading, setLoading] = useState(true);
+  const [menuItems, setMenuItems] = useState([]);
+
+  const getMenu = async () => {
+    try {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu-items-by-category.json"
+      );
+      const json = await response.json();
+      setMenuItems(json.menu);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getMenu();
+  }, []);
+
+  const Item = ({ name, price }) => (
+    <View style={menuStyles.innerContainer}>
+      <Text style={menuStyles.itemText}>{name}</Text>
+      <Text style={menuStyles.itemText}>{"$" + price}</Text>
+    </View>
+  );
+
+  const renderItem = ({ item }) => (
+    <Item name={item.title} price={item.price} />
+  );
 
   return (
     <SafeAreaView style={menuStyles.container}>
-      <Text style={menuStyles.headerText}>Little Lemon</Text>
+      <Text style={menuStyles.headerText}>Little Lemon Menu</Text>
       {isLoading ? (
         <ActivityIndicator />
       ) : (
-        {
-          /* Render flatlist with menu items after fetching from API  here*/
-        }
+        <FlatList
+          data={menuItems}
+          keyExtractor={({ id }, index) => id}
+          renderItem={renderItem}
+        />
       )}
     </SafeAreaView>
   );
@@ -29,9 +61,20 @@ const menuStyles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  innerContainer: {
+    paddingHorizontal: 40,
+    paddingVertical: 20,
+    backgroundColor: "#495E57",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  itemText: {
+    color: "#F4CE14",
+    fontSize: 22,
+  },
   headerText: {
-    color: '#495E57',
+    color: "#F4CE14",
     fontSize: 30,
-    textAlign: 'center',
+    textAlign: "center",
   },
 });
