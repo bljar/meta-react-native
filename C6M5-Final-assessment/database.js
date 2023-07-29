bc11338ecdadf024ebd2e21098973ea676e03608
Belajar <bljkmp@gmail.com>
2023-07-29 10:57:22 +0700

    Store data in SQLite database

--- C6M5-Final-assessment/database.js
+++ C6M5-Final-assessment/database.js
@@ -1,14 +1,14 @@
-import * as SQLite from 'expo-sqlite';
-import { SECTION_LIST_MOCK_DATA } from './utils';
+import * as SQLite from "expo-sqlite";
+import { SECTION_LIST_MOCK_DATA } from "./utils";
 
-const db = SQLite.openDatabase('little_lemon');
+const db = SQLite.openDatabase("little_lemon");
 
 export async function createTable() {
   return new Promise((resolve, reject) => {
     db.transaction(
       (tx) => {
         tx.executeSql(
-          'create table if not exists menuitems (id integer primary key not null, uuid text, title text, price text, category text);'
+          "create table if not exists menuitems (id integer primary key not null, uuid text, title text, price text, category text);"
         );
       },
       reject,
@@ -20,7 +20,7 @@ export async function createTable() {
 export async function getMenuItems() {
   return new Promise((resolve) => {
     db.transaction((tx) => {
-      tx.executeSql('select * from menuitems', [], (_, { rows }) => {
+      tx.executeSql("select * from menuitems", [], (_, { rows }) => {
         resolve(rows._array);
       });
     });
@@ -32,6 +32,14 @@ export function saveMenuItems(menuItems) {
     // 2. Implement a single SQL statement to save all menu data in a table called menuitems.
     // Check the createTable() function above to see all the different columns the table has
     // Hint: You need a SQL statement to insert multiple rows at once.
+    tx.executeSql(
+      `insert into menuitems (uuid, title, price, category) values ${menuItems
+        .map(
+          (item) =>
+            `('${item.id}', '${item.title}', '${item.price}', '${item.category.title}')`
+        )
+        .join(", ")}`
+    );
   });
 }
 
